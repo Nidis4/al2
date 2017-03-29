54bacdaf760c5faf02f4a33d4d2255568757fb51
Nidis4 <macuser@Macusers-MacBook-Pro-10.local>
2017-03-29 12:04:51 +0300

    Changes and Improvements
    Add Colour Function and Select

--- js/mosiko.js
+++ js/mosiko.js
@@ -1,7 +1,28 @@
+var counter = 0;
 $( ".addMS" ).click(function() {
+	counter++;
+  var newID = "mos"+counter; 
   var clicked = this.id;
   var label = clicked.toUpperCase(); 
-  $( ".area" ).append( "<div id='"+clicked+"' class='draggable draggableMS ui-widget-content "+clicked+" ms' style='position:absolute; top:10px'><p>"+label+"</p></div>" );
+
+  var p = $( ".area" );
+  var position = p.position();
+
+  $( ".area" ).append( "<div id='"+newID+"' class='draggable draggableMS ui-widget-content "+clicked+" ms' style='position:absolute; top:" + position.top+"'><p>"+label+"</p></div>" );
   //alert("Button clicked, id "+this.id);
   $( ".draggableMS" ).draggable({ containment: ".area", grid: [ 50, 50 ] });
+});
+
+$( ".addCL" ).click(function() {
+	if ( $( ".draggable.active" ).length ) {
+		$( ".draggable.active" ).css( "background", "black" );
+	}
+	else{
+		alert("Please select an element!");
+	}
+});
+
+
+$(document).on('click', '.draggable', function() { 
+	$( this ).toggleClass( "active" ); 
 });
