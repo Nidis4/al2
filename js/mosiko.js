12808930be701cc61abfac63231333de5e80de6d
Nidis4 <macuser@Macusers-MacBook-Pro-10.local>
2017-03-29 12:37:27 +0300

    Animations

--- js/mosiko.js
+++ js/mosiko.js
@@ -26,3 +26,13 @@ $( ".addCL" ).click(function() {
 $(document).on('click', '.draggable', function() { 
 	$( this ).toggleClass( "active" ); 
 });
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
